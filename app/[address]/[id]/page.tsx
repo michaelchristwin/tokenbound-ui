fa9ca9ec69da373008032ed4194c034896d7ece2
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-12-03 19:06:47 +0100

    Finished UI

--- app/[address]/[id]/page.tsx
+++ app/[address]/[id]/page.tsx
@@ -4,18 +4,23 @@ import { useParams } from "next/navigation";
 import { TokenboundClient } from "@tokenbound/sdk";
 import { WalletClient, createWalletClient, custom, http } from "viem";
 import { goerli } from "viem/chains";
+import ABI from "@/ABIS/ABI.json";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { WindowProvider } from "wagmi";
+import { WindowProvider, useContractRead } from "wagmi";
 import { getAccount } from "wagmi/actions";
 import Image from "next/image";
 declare let window: any;
 
-let contractAddress = "0x4bB0a205fceD93c8834b379c461B07BBe6aAE622";
+let contractAddess = "0x4bB0a205fceD93c8834b379c461B07BBe6aAE622";
+interface Nft {
+  name: string;
+  image: string;
+}
 function Page() {
   const params = useParams();
   const { address } = getAccount();
   const [client, setClient] = useState<WalletClient | undefined>(undefined);
-  console.log(address);
+  const [currentNft, setCurrentNft] = useState<Nft>({ name: "", image: "" });
 
   useEffect(() => {
     if (address && window.ethereum) {
@@ -28,6 +33,25 @@ function Page() {
     }
   }, [address]);
 
+  const { data, isLoading, isError } = useContractRead({
+    address: params.address as `0x${string}`,
+    args: [params.id],
+    functionName: "tokenURI",
+    abi: ABI,
+  });
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const myNft = await fetch(data as string);
+        const res = await myNft.json();
+        const totou: Nft = { name: res.name, image: res.image };
+        setCurrentNft(totou);
+      } catch (err) {
+        console.error("Ferch failed:", err);
+      }
+    })();
+  }, [data]);
   const tokenboundClient = useMemo(() => {
     return new TokenboundClient({
       walletClient: client,
@@ -39,27 +63,27 @@ function Page() {
     if (!tokenboundClient || !address) return;
     try {
       const createdAccount = await tokenboundClient.createAccount({
-        tokenContract: contractAddress as `0x${string}`,
+        tokenContract: params.address as `0x${string}`,
         tokenId: `${params.id}`,
       });
       return createdAccount;
     } catch (err) {
       console.error("Account creation failed", err);
     }
-  }, [tokenboundClient, params.id, address]);
+  }, [tokenboundClient, params.id, params.address, address]);
 
   const getTBAccount = useCallback(() => {
     if (!tokenboundClient || !address) return;
     try {
       const TBAccount = tokenboundClient.getAccount({
-        tokenContract: contractAddress as `0x${string}`,
+        tokenContract: params.address as `0x${string}`,
         tokenId: `${params.id}`,
       });
       return TBAccount;
     } catch (err) {
       console.error("Failed to get account", err);
     }
-  }, [tokenboundClient, params.id, address]);
+  }, [tokenboundClient, params.id, params.address, address]);
 
   const isAccountDeployed = useCallback(async () => {
     if (!tokenboundClient || !address) return;
@@ -91,41 +115,42 @@ function Page() {
     >
       <div className={`block space-y-3`}>
         <Image
-          src={`/F1daXLaaIAANzSM.jpg`}
+          src={currentNft.image}
           width={300}
           height={400}
           className={`w-[350px] h-[400px] rounded-[15px]`}
           alt="NFT"
         />
-        <p className={`text-[22px] font-[600]`}>Chronicles of Narnia</p>
       </div>
-      <div className={`block space-y-3`}>
-        <h1>999 {params.address}</h1>
-        <h1>{params.id}</h1>
-        <button
-          className={`px-2 block h-[33px] bg-white text-black rounded-[13px]`}
-          onClick={createAccount}
-        >
-          Create Account
-        </button>
-        <button
-          className={`px-2 h-[33px] block bg-white text-black rounded-[13px]`}
-          onClick={getTBAccount}
-        >
-          Get Account
-        </button>
-        <button
-          className={`px-2 h-[33px] block bg-white text-black rounded-[13px]`}
-          onClick={isAccountDeployed}
-        >
-          Is Deployed
-        </button>
-        <button
-          className={`px-2 h-[33px] block bg-white text-black rounded-[13px]`}
-          onClick={getNft}
-        >
-          Get NFT
-        </button>
+      <div className={`block`}>
+        <p className={`text-[22px] font-[600]`}>{currentNft.name}</p>
+
+        <div className={`block space-y-3 items-center mt-[100px]`}>
+          <button
+            className={`px-2 block h-[33px] bg-white text-black rounded-[13px]`}
+            onClick={createAccount}
+          >
+            Create Account
+          </button>
+          <button
+            className={`px-2 h-[33px] block bg-white text-black rounded-[13px]`}
+            onClick={getTBAccount}
+          >
+            Get Account
+          </button>
+          <button
+            className={`px-2 h-[33px] block bg-white text-black rounded-[13px]`}
+            onClick={isAccountDeployed}
+          >
+            Is Deployed
+          </button>
+          <button
+            className={`px-2 h-[33px] block bg-white text-black rounded-[13px]`}
+            onClick={getNft}
+          >
+            Get NFT
+          </button>
+        </div>
       </div>
     </div>
   );
